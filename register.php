182608cb3f745bc6b9cd949ce12be66248cdf712
MuhFadilFahrozi <fadilfahrozi1945@gmail.com>
2023-11-06 23:25:26 +0700

    UAS BENKOD

--- register.php
+++ register.php
@@ -42,7 +42,7 @@ if (isset($_POST['submit'])){
 <!DOCTYPE html>
 <html>
 <head>
-<title>Creative Colorlib SignUp Form</title>
+<title>Form registrasi akun</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
